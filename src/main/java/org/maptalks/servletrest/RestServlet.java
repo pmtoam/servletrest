1a3417f750421860b6b62d511b1b9b99e0abf312
wsw <wsw0108@gmail.com>
2016-05-31 18:33:37 +0800

    override init() instead

--- src/main/java/org/maptalks/servletrest/RestServlet.java
+++ src/main/java/org/maptalks/servletrest/RestServlet.java
@@ -1,9 +1,7 @@
 package org.maptalks.servletrest;
 
-import java.io.IOException;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Locale;
+import org.maptalks.servletrest.config.ServletPattern;
+import org.maptalks.servletrest.config.exceptions.ServletFactoryNotReadyException;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -11,53 +9,52 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-
-import org.maptalks.servletrest.config.ServletPattern;
-import org.maptalks.servletrest.config.exceptions.ServletFactoryNotReadyException;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Locale;
 
 public class RestServlet extends HttpServlet {
 	private static ServletFactory servletFactory;
 	private static String encoding;
+	private static final String DEFAULT_ENCODING = "UTF-8";
 
 	@Override
-	public void init(ServletConfig config) throws ServletException {
-		try {
-			System.out.println("[RestServlet] begin to initialize servlets");
-			final String servletConfigPath = config
-					.getInitParameter("ServletConfig");
-			System.out.println("[RestServlet] servlet config:"
-					+ servletConfigPath);
-			servletFactory = new ServletFactory();
-			servletFactory.init(servletConfigPath, config);
-			List<ServletPattern> patterns = servletFactory.getServletPatterns();
-			if (patterns != null) {
-				for (Iterator iterator = patterns.iterator(); iterator
-						.hasNext();) {
-					ServletPattern servletPattern = (ServletPattern) iterator
-							.next();
-					System.out.println("[RestServlet] servlet pattern:"
-							+ servletPattern.getPattern());
-				}
-			} else {
-				System.out.println("[RestServlet] no servlet pattern found.;");
+	public void init() throws ServletException {
+		System.out.println("[RestServlet] begin to initialize servlets");
+		ServletConfig config = getServletConfig();
+		final String servletConfigPath = getInitParameter("ServletConfig");
+		System.out.println("[RestServlet] servlet config:"
+			+ servletConfigPath);
+		servletFactory = new ServletFactory();
+		servletFactory.init(servletConfigPath, config);
+		List<ServletPattern> patterns = servletFactory.getServletPatterns();
+		if (patterns != null) {
+			for (Iterator iterator = patterns.iterator(); iterator
+				.hasNext(); ) {
+				ServletPattern servletPattern = (ServletPattern) iterator
+					.next();
+				System.out.println("[RestServlet] servlet pattern:"
+					+ servletPattern.getPattern());
 			}
-			encoding = config.getInitParameter("Encoding");
-			System.out.println("[RestServlet] initializing ended;");
-			super.init(config);
-		} catch (Throwable e) {
-			e.printStackTrace();
+		} else {
+			System.out.println("[RestServlet] no servlet pattern found.;");
 		}
+		encoding = getInitParameter("Encoding");
+		if (encoding == null) {
+			encoding = DEFAULT_ENCODING;
+		}
+		System.out.println("[RestServlet] initializing ended;");
 	}
 
 	@Override
 	public void service(ServletRequest req, ServletResponse rep)
-			throws ServletException, IOException {
+		throws ServletException, IOException {
 		final HttpServletRequest request = (HttpServletRequest) req;
 
 		final String realUrl = request.getRequestURI().replaceFirst(
-				request.getContextPath(), "");
-		//		System.out.println("[RestServlet] hit" + realUrl);
-		HttpServlet servlet = null;
+			request.getContextPath(), "");
+		HttpServlet servlet;
 		try {
 			servlet = servletFactory.getServletByUrl(realUrl);
 		} catch (final ServletFactoryNotReadyException e) {
@@ -66,14 +63,8 @@ public class RestServlet extends HttpServlet {
 		if (servlet != null) {
 			rep.setCharacterEncoding(encoding);
 			rep.setLocale(new Locale("zh"));
-			try {
-				servlet.service(req, rep);
-			} catch (Throwable e) {
-				//				e.printStackTrace();
-				throw new ServletException(e);
-			}
+			servlet.service(req, rep);
 		} else {
-			//			System.out.println("[RestServlet] no servlet serviced!");
 			super.service(req, rep);
 		}
 
